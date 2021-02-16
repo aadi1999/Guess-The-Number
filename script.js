4549153d4a045bdcbf527eefc6ed70769af68d09
Aadi Shakti Tripathi <aadi1999>
2021-02-16 15:49:37 +0530

    implemented high score logic

--- script.js
+++ script.js
@@ -5,12 +5,22 @@
 // console.log(document.querySelector('.guess').value);
 // console.log(document.querySelector(".check"));
 
-const randomNumber = Math.trunc(Math.random()*20 + 1);
-
-document.querySelector(".number").textContent = randomNumber;
-
-
+let randomNumber = Math.trunc(Math.random()*20 + 1);
+let highscore = 0;
 let score = 20;
+
+document.querySelector(".again").addEventListener('click',function(){
+
+    document.querySelector(".message").textContent = "Start Guessing...";
+    score = 20;
+    randomNumber = Math.trunc(Math.random()*20 + 1);
+    document.querySelector(".score").textContent = score;
+    document.querySelector(".number").textContent = "?";
+    document.querySelector("body").style.backgroundColor = " #222"
+    document.querySelector(".guess").value = '';
+    
+    // document.querySelector(".number").textContent = randomNumber;
+})
 
 document.querySelector('.check').addEventListener('click',function(){
 
@@ -21,8 +31,14 @@ document.querySelector('.check').addEventListener('click',function(){
     if(!guess){
         document.querySelector(".message").textContent = "⚠ No Number";
     }else if(guess === randomNumber){
+        document.querySelector(".number").textContent = randomNumber;
         document.querySelector(".message").textContent = "😊Correct Number";
         document.querySelector("body").style.backgroundColor = "#60b347";
+        if(score > highscore){
+            highscore = score;
+            document.querySelector(".highscore").textContent = highscore;
+        }
+        
     }else if(guess > randomNumber){
         if(score > 1){
             document.querySelector(".message").textContent = " Too high";
